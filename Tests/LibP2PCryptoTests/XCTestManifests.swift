ddbd15596a151af79e7c6b2f5030ea93fa6bab42
Brandon Toms <btoms.20@gmail.com>
2022-05-17 14:20:37 -0700

    Bumped SPM version to 5.4

--- Tests/LibP2PCryptoTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(libp2p_cryptoTests.allTests),
-    ]
-}
-#endif
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import libp2p_cryptoTests
-
-var tests = [XCTestCaseEntry]()
-tests += libp2p_cryptoTests.allTests()
-XCTMain(tests)
