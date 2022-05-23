25574652db39d2f9dde0a6598a1a729374b63649
Brandon Toms <btoms.20@gmail.com>
2022-05-23 15:10:02 -0700

    HMACs now use swift-crypto instead of CommonCrypto

--- Sources/LibP2PCrypto/HMAC/HMAC.swift
+++ Sources/LibP2PCrypto/HMAC/HMAC.swift
@@ -1,43 +1,42 @@
 //
 //  HMAC.swift
-//  
+//
 //
 //  Created by Brandon Toms on 5/1/22.
 //
 
 import Foundation
-import CommonCrypto
 import Multibase
+import Crypto
 
 extension LibP2PCrypto {
     public enum HMAC {
         public enum CryptoAlgorithm {
-            case MD5, SHA1, SHA224, SHA256, SHA384, SHA512
+            case MD5, SHA1, SHA256, SHA384, SHA512
 
-            var HMACAlgorithm: CCHmacAlgorithm {
-                var result: Int = 0
+            internal func encrypt(_ message:Data, key: String) -> Data {
                 switch self {
-                case .MD5:    result = kCCHmacAlgMD5
-                case .SHA1:   result = kCCHmacAlgSHA1
-                case .SHA224: result = kCCHmacAlgSHA224
-                case .SHA256: result = kCCHmacAlgSHA256
-                case .SHA384: result = kCCHmacAlgSHA384
-                case .SHA512: result = kCCHmacAlgSHA512
+                case .MD5:
+                    var hmac = Crypto.HMAC<Insecure.MD5>(key: SymmetricKey(data: key.bytes))
+                    hmac.update(data: message)
+                    return Data(hmac.finalize())
+                case .SHA1:
+                    var hmac = Crypto.HMAC<Insecure.SHA1>(key: SymmetricKey(data: key.bytes))
+                    hmac.update(data: message)
+                    return Data(hmac.finalize())
+                case .SHA256:
+                    var hmac = Crypto.HMAC<SHA256>(key: SymmetricKey(data: key.bytes))
+                    hmac.update(data: message)
+                    return Data(hmac.finalize())
+                case .SHA384:
+                    var hmac = Crypto.HMAC<SHA384>(key: SymmetricKey(data: key.bytes))
+                    hmac.update(data: message)
+                    return Data(hmac.finalize())
+                case .SHA512:
+                    var hmac = Crypto.HMAC<SHA512>(key: SymmetricKey(data: key.bytes))
+                    hmac.update(data: message)
+                    return Data(hmac.finalize())
                 }
-                return CCHmacAlgorithm(result)
-            }
-
-            var digestLength: Int {
-                var result: Int32 = 0
-                switch self {
-                case .MD5:    result = CC_MD5_DIGEST_LENGTH
-                case .SHA1:   result = CC_SHA1_DIGEST_LENGTH
-                case .SHA224: result = CC_SHA224_DIGEST_LENGTH
-                case .SHA256: result = CC_SHA256_DIGEST_LENGTH
-                case .SHA384: result = CC_SHA384_DIGEST_LENGTH
-                case .SHA512: result = CC_SHA512_DIGEST_LENGTH
-                }
-                return Int(result)
             }
         }
         
@@ -98,22 +97,7 @@ extension LibP2PCrypto {
         ///   - key: The shared secret key to hash the data against
         /// - Returns: The encrypted / hashed HMAC data
         public static func encrypt(_ message:Data, algorithm: CryptoAlgorithm, key: String) -> Data {
-            var data = message
-            let dLen = data.count
-            let digestLen = algorithm.digestLength
-            let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLen)
-            let keyStr = key.cString(using: String.Encoding.utf8)
-            let keyLen = Int(key.lengthOfBytes(using: String.Encoding.utf8))
-
-            CCHmac(algorithm.HMACAlgorithm, keyStr!, keyLen, &data, dLen, result)
-
-            var d:[UInt8] = []
-            for i in 0..<digestLen {
-                d.append(result[i])
-            }
-            
-            result.deallocate()
-            return Data(d)
+            return algorithm.encrypt(message, key: key)
         }
     }
 }
@@ -194,5 +178,4 @@ public extension Decryptable {
         return str
     }
 }
-
 
