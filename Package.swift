de59b92447bb6c14385075c49f8e070e35c5ad0a
Brandon Toms <btoms.20@gmail.com>
2022-09-18 10:40:35 -0700

    Bumped CryptoSwift version to 1.6.+

--- Package.swift
+++ Package.swift
@@ -17,17 +17,15 @@ let package = Package(
     ],
     dependencies: [
         // Multibase Support
-        .package(url: "https://github.com/swift-libp2p/swift-multibase.git", .upToNextMajor(from: "0.0.1")),
+        .package(url: "https://github.com/swift-libp2p/swift-multibase.git", .upToNextMinor(from: "0.0.1")),
         // Protobuf Marshaling
         .package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.12.0")),
         // Secp256k1 Support
         .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", .exact("0.1.6")),
         // 🔑 Hashing (BCrypt, SHA2, HMAC), encryption (AES), public-key (RSA), PEM and DER file handling, and random data generation.
         .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .exact("1.5.1")),
-        .package(url: "https://github.com/swift-libp2p/swift-multihash.git", .upToNextMajor(from: "0.0.1")),
-        // JWT Support
-        //.package(url: "https://github.com/vapor/jwt-kit.git", .upToNextMajor(from: "4.0.0"))
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.6.0")),
+        .package(url: "https://github.com/swift-libp2p/swift-multihash.git", .upToNextMinor(from: "0.0.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -41,7 +39,6 @@ let package = Package(
                 .product(name: "secp256k1", package: "secp256k1.swift"),
                 .product(name: "Crypto", package: "swift-crypto"),
                 .product(name: "CryptoSwift", package: "CryptoSwift"),
-                //.product(name: "JWTKit", package: "jwt-kit")
             ],
             resources: [
               .copy("Protobufs/keys.proto")
