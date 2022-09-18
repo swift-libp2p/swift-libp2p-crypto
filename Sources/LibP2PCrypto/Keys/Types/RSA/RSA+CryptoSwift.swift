8b67e10848f784252ad5585f99b79a5887423f1e
Brandon Toms <btoms.20@gmail.com>
2022-09-18 00:21:37 -0700

    Removed unused code

--- Sources/LibP2PCrypto/Keys/Types/RSA/RSA+CryptoSwift.swift
+++ Sources/LibP2PCrypto/Keys/Types/RSA/RSA+CryptoSwift.swift
@@ -126,8 +126,8 @@ struct RSAPrivateKey:CommonPrivateKey {
     }
     
     var rawRepresentation: Data {
-        guard key.d != nil else { /*throw NSError(domain: "Not a valid private RSA Key", code: 0)*/ return Data() }
-        return try! self.key.externalRepresentation()
+        guard key.d != nil, let raw = try? self.key.externalRepresentation() else { return Data() }
+        return raw
     }
     
     func derivePublicKey() throws -> CommonPublicKey {
@@ -170,17 +170,7 @@ extension CryptoSwift.RSA {
     /// - Note: The signature uses the SHA256 PKCS#1v15 Padding Scheme
     /// - Note: [EMSA-PKCS1-v1_5](https://datatracker.ietf.org/doc/html/rfc8017#section-9.2)
     fileprivate static func sign(message:Data, withKey key:RSA) throws -> Data {
-        
         return try Data(key.sign(message.bytes, variant: .message_pkcs1v15_SHA256))
-        
-//        guard let d = key.d else { throw NSError(domain: "Signing data requires a Private RSA key", code: 0) }
-//        let encodedMessage = try CryptoSwift.RSA.hashedAndPKCSEncoded(message.bytes, modLength: key.n.serialize().count)
-//
-//        let n = BigUInteger(Data(encodedMessage))
-//        let e = d
-//        let m = key.n
-//        let signedData_RsaKey = CryptoSwift.RSA.modPow(n: n, e: e, m: m).serialize()
-//        return signedData_RsaKey
     }
     
     /// Verifies a signature for the expected data
@@ -188,97 +178,8 @@ extension CryptoSwift.RSA {
     /// - Note: This method assumes the signature was generated using the SHA256 PKCS#1v15 Padding Scheme
     /// - Note: [EMSA-PKCS1-v1_5](https://datatracker.ietf.org/doc/html/rfc8017#section-9.2)
     fileprivate static func verify(signature:Data, fromMessage message:Data, usingKey key:RSA) throws -> Bool {
-        
         return try key.verify(signature: signature.bytes, for: message.bytes, variant: .message_pkcs1v15_SHA256)
-        
-//        let modLength = key.n.serialize().count
-//        /// Step 1: Ensure the signature is the same length as the key's modulus
-//        guard signature.count == modLength else { throw NSError(domain: "Invalid Signature Length", code: 0) }
-//
-//        /// Step 2: 'Decrypt' the signature
-//        let n = BigUInteger(signature)
-//        let e = key.e
-//        let m = key.n
-//        let pkcsEncodedSHA256HashedMessage = CryptoSwift.RSA.modPow(n: n, e: e, m: m).serialize()
-//
-//        /// Step 3: Compare the 'decrypted' signature with the prepared / encoded expected message....
-//        let preparedExpectedMessage = try CryptoSwift.RSA.hashedAndPKCSEncoded(message.bytes, modLength: modLength).dropFirst()
-//
-//        guard pkcsEncodedSHA256HashedMessage == preparedExpectedMessage else { return false }
-//
-//        return true
     }
-    
-    /// prepends the data with zero's until it reaches the specified length
-//    fileprivate static func zeroPad(n:[UInt8], to:Int) -> [UInt8] {
-//        var modulus = n
-//        while modulus.count < to {
-//            modulus.insert(0x00, at: 0)
-//        }
-//        return modulus
-//    }
-    
-    /// Modular exponentiation
-    ///
-    /// - Credit: AttaSwift BigInt
-    /// - Source: https://rosettacode.org/wiki/Modular_exponentiation#Swift
-//    fileprivate static func modPow<T: BinaryInteger>(n: T, e: T, m: T) -> T {
-//        guard e != 0 else {
-//            return 1
-//        }
-//
-//        var res = T(1)
-//        var base = n % m
-//        var exp = e
-//
-//        while true {
-//            if exp & 1 == 1 {
-//                res *= base
-//                res %= m
-//            }
-//
-//            if exp == 1 {
-//                return res
-//            }
-//
-//            exp /= 2
-//            base *= base
-//            base %= m
-//        }
-//    }
-    
-    /// Hashes and Encodes a message for signing and verifying
-    ///
-    /// - Note: [EMSA-PKCS1-v1_5](https://datatracker.ietf.org/doc/html/rfc8017#section-9.2)
-//    fileprivate static func hashedAndPKCSEncoded(_ message:[UInt8], modLength:Int) throws -> Data {
-//        /// 1.  Apply the hash function to the message M to produce a hash
-//        let hashedMessage = SHA2(variant: .sha256).calculate(for: message)
-//
-//        /// 2. Encode the algorithm ID for the hash function and the hash value into an ASN.1 value of type DigestInfo
-//        /// PKCS#1_15 DER Structure (OID == sha256WithRSAEncryption)
-//        let asn:ASN1.Node = .sequence(nodes: [
-//            .sequence(nodes: [
-//                .objectIdentifier(data: Data(Array<UInt8>(arrayLiteral: 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01))),
-//                .null
-//            ]),
-//            .octetString(data: Data(hashedMessage))
-//        ])
-//
-//        let t = ASN1.Encoder.encode(asn)
-//
-//        /// 3.  If emLen < tLen + 11, output "intended encoded message lengthtoo short" and stop
-//        if modLength < t.count + 11 { throw NSError(domain: "intended encoded message length too short", code: 0) }
-//
-//        /// 4.  Generate an octet string PS consisting of emLen - tLen - 3
-//        /// octets with hexadecimal value 0xff. The length of PS will be
-//        /// at least 8 octets.
-//        let r = modLength - t.count - 3
-//        let padding = [0x00, 0x01] + Array<UInt8>(repeating: 0xFF, count: r) + [0x00]
-//
-//        /// 5.  Concatenate PS, the DER encoding T, and other padding to form
-//        /// the encoded message EM as EM = 0x00 || 0x01 || PS || 0x00 || T.
-//        return Data(padding + t)
-//    }
 }
 
 #endif
