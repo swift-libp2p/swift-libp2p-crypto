f97eee1d645ad03670cd21f91a86d6760f94d385
Brandon <32753167+btoms20@users.noreply.github.com>
2025-03-04 21:57:23 -0800

    Reverted protobuf init

--- Tests/LibP2PCryptoTests/LibP2PCryptoTests.swift
+++ Tests/LibP2PCryptoTests/LibP2PCryptoTests.swift
@@ -321,7 +321,7 @@ final class libp2p_cryptoTests: XCTestCase {
     func testSecp256k1ParseCompressedPublicKey() throws {
         let marshalledCompressedPublicKey = "08021221037777e994e452c21604f91de093ce415f5432f701dd8cd1a7a6fea0e630bfca99"
 
-        let pb = try PublicKey(serializedBytes: Data(hex: marshalledCompressedPublicKey))
+        let pb = try PublicKey(contiguousBytes: Data(hex: marshalledCompressedPublicKey))
 
         let pubKey = try Secp256k1PublicKey(publicKey: pb.data.bytes)
 
@@ -346,7 +346,7 @@ final class libp2p_cryptoTests: XCTestCase {
         let expectedRSAPublicKey =
             "080012a60430820222300d06092a864886f70d01010105000382020f003082020a0282020100e1beab071d08200bde24eef00d049449b07770ff9910257b2d7d5dda242ce8f0e2f12e1af4b32d9efd2c090f66b0f29986dbb645dae9880089704a94e5066d594162ae6ee8892e6ec70701db0a6c445c04778eb3de1293aa1a23c3825b85c6620a2bc3f82f9b0c309bc0ab3aeb1873282bebd3da03c33e76c21e9beb172fd44c9e43be32e2c99827033cf8d0f0c606f4579326c930eb4e854395ad941256542c793902185153c474bed109d6ff5141ebf9cd256cf58893a37f83729f97e7cb435ec679d2e33901d27bb35aa0d7e20561da08885ef0abbf8e2fb48d6a5487047a9ecb1ad41fa7ed84f6e3e8ecd5d98b3982d2a901b4454991766da295ab78822add5612a2df83bcee814cf50973e80d7ef38111b1bd87da2ae92438a2c8cbcc70b31ee319939a3b9c761dbc13b5c086d6b64bf7ae7dacc14622375d92a8ff9af7eb962162bbddebf90acb32adb5e4e4029f1c96019949ecfbfeffd7ac1e3fbcc6b6168c34be3d5a2e5999fcbb39bba7adbca78eab09b9bc39f7fa4b93411f4cc175e70c0a083e96bfaefb04a9580b4753c1738a6a760ae1afd851a1a4bdad231cf56e9284d832483df215a46c1c21bdf0c6cfe951c18f1ee4078c79c13d63edb6e14feaeffabc90ad317e4875fe648101b0864097e998f0ca3025ef9638cd2b0caecd3770ab54a1d9c6ca959b0f5dcbc90caeefc4135baca6fd475224269bbe1b0203010001"
 
-        let pbPrivateKey = try PrivateKey(serializedBytes: Data(hex: rsaPrivateKey))
+        let pbPrivateKey = try PrivateKey(contiguousBytes: Data(hex: rsaPrivateKey))
         XCTAssertEqual(pbPrivateKey.type, .rsa)
 
         let kp = try LibP2PCrypto.Keys.KeyPair(marshaledPrivateKey: Data(hex: rsaPrivateKey))
@@ -360,7 +360,7 @@ final class libp2p_cryptoTests: XCTestCase {
             "080112407e0830617c4a7de83925dfb2694556b12936c477a0e1feb2e148ec9da60fee7d1ed1e8fae2c4a144b8be8fd4b47bf3d3b34b871c3cacf6010f0e42d474fce27e"
         let expectedED25519PublicKey = "080112201ed1e8fae2c4a144b8be8fd4b47bf3d3b34b871c3cacf6010f0e42d474fce27e"
 
-        let pbPrivateKey = try PrivateKey(serializedBytes: Data(hex: ed25519PrivateKey))
+        let pbPrivateKey = try PrivateKey(contiguousBytes: Data(hex: ed25519PrivateKey))
         XCTAssertEqual(pbPrivateKey.type, .ed25519)
 
         let kp = try LibP2PCrypto.Keys.KeyPair(marshaledPrivateKey: Data(hex: ed25519PrivateKey))
@@ -374,7 +374,7 @@ final class libp2p_cryptoTests: XCTestCase {
         let secp256k1PrivateKey = "0802122053DADF1D5A164D6B4ACDB15E24AA4C5B1D3461BDBD42ABEDB0A4404D56CED8FB"
         let expectedSecp256k1PublicKey = "08021221037777e994e452c21604f91de093ce415f5432f701dd8cd1a7a6fea0e630bfca99"
 
-        let pbPrivateKey = try PrivateKey(serializedBytes: Data(hex: secp256k1PrivateKey))
+        let pbPrivateKey = try PrivateKey(contiguousBytes: Data(hex: secp256k1PrivateKey))
         XCTAssertEqual(pbPrivateKey.type, .secp256K1)
 
         let kp = try LibP2PCrypto.Keys.KeyPair(marshaledPrivateKey: Data(hex: secp256k1PrivateKey))
