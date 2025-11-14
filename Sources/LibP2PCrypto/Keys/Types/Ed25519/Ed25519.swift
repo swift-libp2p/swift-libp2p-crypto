b3fc9d65cb7f2c8112e86173489c106f4ca5b5ae
Brandon <32753167+btoms20@users.noreply.github.com>
2025-11-14 14:08:12 -0800

    remove unused code

--- Sources/LibP2PCrypto/Keys/Types/Ed25519/Ed25519.swift
+++ Sources/LibP2PCrypto/Keys/Types/Ed25519/Ed25519.swift
@@ -15,8 +15,6 @@
 import Crypto
 import Foundation
 
-//import PEM
-
 extension Curve25519.Signing.PublicKey: CommonPublicKey, @unchecked Sendable {
     public static var keyType: LibP2PCrypto.Keys.GenericKeyType { .ed25519 }
 
@@ -38,34 +36,6 @@ extension Curve25519.Signing.PublicKey: CommonPublicKey, @unchecked Sendable {
         publicKey.data = self.rawRepresentation
         return try publicKey.serializedData()
     }
-
-    //    public init(pem:String) throws {
-    //        let chunks = pem.split(separator: "\n")
-    //        guard chunks.count > 3,
-    //              let f = chunks.first, f.hasPrefix("-----BEGIN"),
-    //              let l = chunks.last, l.hasSuffix("-----") else {
-    //            throw NSError(domain: "Invalid PEM Format", code: 0, userInfo: nil)
-    //        }
-    //
-    //        //print("Attempting to decode: \(chunks[1..<chunks.count-1].joined())")
-    //        let raw = try BaseEncoding.decode(chunks[1..<chunks.count-1].joined(), as: .base64)
-    //        //print(raw.data)
-    //
-    //        //let key = try LibP2PCrypto.Keys.stripKeyHeader(keyData: raw.data)
-    //
-    //        let asn1 = try LibP2PCrypto.Keys.parseASN1(pemData: raw.data)
-    //
-    //        guard asn1.isPrivateKey == false else {
-    //            throw NSError(domain: "The provided PEM isn't a Public Key. Try importPrivatePem() instead...", code: 0, userInfo: nil)
-    //        }
-    //
-    //        if asn1.objectIdentifier.prefix(5) == Data([0x2a, 0x86, 0x48, 0xce, 0x3d]) {
-    //            print("Trying to Init EC Key")
-    //            self = try Curve25519.Signing.PublicKey(rawRepresentation: asn1.keyBits)
-    //        }
-    //
-    //        throw NSError(domain: "Failed to parse PEM into known key type \(asn1)", code: 0, userInfo: nil)
-    //    }
 }
 
 extension Curve25519.Signing.PrivateKey: CommonPrivateKey, @unchecked Sendable {
@@ -93,36 +63,6 @@ extension Curve25519.Signing.PrivateKey: CommonPrivateKey, @unchecked Sendable {
         privateKey.data = self.rawRepresentation
         return try privateKey.serializedData()
     }
-
-    //    public init(pemRSA:String) throws { throw NSError(domain: "EC Key Can't be initialized with an RSA PEM", code: 0, userInfo: nil) }
-    //    public init(pemEC:String) throws {
-    //        let chunks = pemEC.split(separator: "\n")
-    //        guard chunks.count > 3,
-    //              let f = chunks.first, f.hasPrefix("-----BEGIN PRIVATE"),
-    //              let l = chunks.last, l.hasSuffix("-----") else {
-    //            throw NSError(domain: "Invalid PEM Format", code: 0, userInfo: nil)
-    //        }
-    //
-    //        //print("Attempting to decode: \(chunks[1..<chunks.count-1].joined())")
-    //        let raw = try BaseEncoding.decode(chunks[1..<chunks.count-1].joined(), as: .base64)
-    //        //print(raw.data)
-    //
-    //        var octet = try LibP2PCrypto.Keys.parseASN1ECPrivate(pemData: raw.data)
-    //
-    //        let bytes = octet.bytes.count
-    //
-    //        // TODO Create a PublicKey and PrivateKey Protocol that SecKey and these keys can conform to so we can return a common type...
-    //        if bytes <= 32 { //P256 Pivate Key
-    //            while octet.bytes.count < 32 { octet.insert(0, at: 0) }
-    //            self = try Curve25519.Signing.PrivateKey(rawRepresentation: octet)
-    //        }
-    //
-    //        throw NSError(domain: "Failed to parse PEM into known key type \(octet)", code: 0, userInfo: nil)
-    //    }
-    //
-    //    public func derivePublicKey() throws -> CommonPublicKey {
-    //        self.publicKey
-    //    }
 }
 
 extension Curve25519.Signing.PublicKey: @retroactive Equatable {
